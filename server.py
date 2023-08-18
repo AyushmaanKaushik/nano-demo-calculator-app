7fcc37d4eb2bd6bc5306df73c79e58d39e99233b
AyushmaanKaushik <ayushmaan1362@gmail.com>
2023-08-18 20:19:01 +0530

    Implemented calculator api

--- server.py
+++ server.py
@@ -1,19 +1,20 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    return str(request.json['first'] + request.json['second'])
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    return str(request.json['first'] - request.json['second'])
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
